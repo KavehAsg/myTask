e278cb61dc497300d815ddab3c2b4802bc3c6dae
kaveh <kaveh.asgari1382@gmail.com>
2025-06-05 14:19:36 +0330

    fix orders api

--- services/getOrders2.js
+++ services/getOrders2.js
@@ -1,6 +1,6 @@
 export default async function getOrders2() {
     try {
-        const res = await fetch(`http://localhost:4000/orders`)
+        const res = await fetch(`/.netlify/functions/orders`)
 
         const data = await res.json();
         return data;
